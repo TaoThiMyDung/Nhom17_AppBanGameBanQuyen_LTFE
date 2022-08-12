390ed8acd347438e75b941eca93decda185eb39b
DauThiHoa <91518695+DauThiHoa@users.noreply.github.com>
2022-08-12 17:14:53 +0700

    TestProductDetails
    Chi Tiết Sản Phẩm

--- src/app/components/product-detail/product-detail.component.ts
+++ src/app/components/product-detail/product-detail.component.ts
@@ -11,9 +11,12 @@ import {Product} from "../../models/product";
 })
 export class ProductDetailComponent implements OnInit {
   id: number = 0 ;
+  category :string = "";
+
   // Doi Tuong Product Duoc Bam Vao
   pro : Product = new Product(); // any ( Nhan Tat Ca Cac Du Lieu )
   relatedProduct : Array<Product> = new Array<Product>();
+  related : Array<Product> = new Array<Product>();
 
   constructor(private  prodSrv : ProductService ,
               private _route :ActivatedRoute,
@@ -27,13 +30,25 @@ export class ProductDetailComponent implements OnInit {
     // LAY DU LIEU TREN THANH TIM KIEM
     // /productDetail/1 => id = 1
     this.id = this._route.snapshot.params.id ;
+    this.category = this._route.snapshot.params.category ;
 
     this.prodSrv.getOne(this.id).subscribe(data =>{
       this.pro = data ;
     })
-    this.productService.getRelatedProduct()
+    this.prodSrv.getOne(this.id).subscribe(data =>{
+      this.category = data.category;
+      this.productService.getRelatedProduct(4,this.category)
       .subscribe(res => {
         this.relatedProduct = res;
+       })
+      });
+        this.prodSrv.getOne(this.id).subscribe(data =>{
+          this.category = data.category;
+          this.productService.getRelated(this.category)
+            .subscribe(res => {
+              this.related = res;
+              // alert(this.category)
+        })
       });
   }
 }
